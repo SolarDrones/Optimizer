f799e499ca2a2fc232dfd48836889802e0ea6ba0
Matt Borland <matt.borland1@gmail.com>
2015-05-12 02:36:42 -0400

    Changed for debugging of atmospheric conditions file

--- src/main.py
+++ src/main.py
@@ -17,28 +17,29 @@ class SolarOptimization(Assembly):
 
         # Create components and their parameters
         # Airfoil Geometry
-        self.add('airfoil', Airfoil_Geometry())
+        #self.add('airfoil', Airfoil_Geometry())
         # Constrain the airfcraft to fly between 5 and 15 m/s
-        self.driver.add_parameter('airfoil.u_inf', low=5., high=15.)
+        #self.driver.add_parameter('airfoil.u_inf', low=5., high=15.)
 
         # Constrain the angle of attack to be between -2 and 8 degrees
-        self.driver.add_parameter('airfoil.alpha', low=-2., high=8.)
+        #self.driver.add_parameter('airfoil.alpha', low=-2., high=8.)
 
         # Constrain the y coordinates of the airfoil to be between -1 and 1
         # Also add a 1000x scaler since the size is orders of magnitude smaller
-        self.driver.add_parameter('airfoil.y', low=-1., high=1., scaler=1000.)
+        #self.driver.add_parameter('airfoil.y', low=-1., high=1., scaler=1000.)
 
         # Atmospheric Conditions
         self.add('conditions', Atmospheric_Conditions())
 
-        # Constrain the operating altitude to be between 10 and 2500m
-        self.driver.add_parameter('conditions.altitude', low=10., high=2500.)
+        # Constrain the operating altitude to be between 10 and 3000m
+        self.driver.add_parameter('conditions.altitude', low=10., high=3000.)
 
         # Iteration Hierarchy
-        self.driver.workflow.add(['airfoil', 'conditions'])
+        #self.driver.workflow.add(['airfoil', 'conditions'])
+        self.driver.workflow.add('conditions')
 
         # Objective
-        self.driver.add_objective('airfoil.cl')
+        self.driver.add_objective('conditions.rho')
 
 
 
@@ -55,5 +56,5 @@ if __name__ == '__main__':
 
     # Print the results
     print "\n"
-    print "Solution: %f" % (opt_problem.airfoil.cl)
+    print "Solution: %f" % (opt_problem.conditions.rho)
     print "Elapsed time: ", time.time()-tt, "seconds"
