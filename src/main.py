5fc3e36f8244147b6ab70f695fbf8360a6ba72c3
Matt Borland <matt.borland1@gmail.com>
2015-05-13 21:01:11 -0400

    Removed separate airfoil geometry parts

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 # This is the main and assembly for the solar optimization problem
 
 import time
-from airfoil_geometry import Airfoil_Geometry
 from atmospheric_conditions import Atmospheric_Conditions
+from geometry import Geometry
 from openmdao.main.api import Assembly
 from openmdao.lib.drivers.api import CONMINdriver
 
@@ -16,27 +16,43 @@ class SolarOptimization(Assembly):
         self.driver.itmax = 30      # Max number of iterations
 
         # Create components and their parameters
-        # Airfoil Geometry
-        #self.add('airfoil', Airfoil_Geometry())
-        # Constrain the airfcraft to fly between 5 and 15 m/s
-        #self.driver.add_parameter('airfoil.u_inf', low=5., high=15.)
-
-        # Constrain the angle of attack to be between -2 and 8 degrees
-        #self.driver.add_parameter('airfoil.alpha', low=-2., high=8.)
-
-        # Constrain the y coordinates of the airfoil to be between -1 and 1
-        # Also add a 1000x scaler since the size is orders of magnitude smaller
-        #self.driver.add_parameter('airfoil.y', low=-1., high=1., scaler=1000.)
 
         # Atmospheric Conditions
         self.add('conditions', Atmospheric_Conditions())
 
         # Constrain the operating altitude to be between 10 and 3000m
-        self.driver.add_parameter('conditions.altitude', low=10., high=3000.)
+        self.driver.add_parameter('conditions.altitude', low=10., high=3000., scaler=.001)
+
+
+
+
+        # Aircraft Geometry
+        self.add('geometry', Geometry())
+
+        # Constrain the y coordinates of the airfoil to be between -1 and 1
+        self.driver.add_parameter('geometry.y_foil', low-1., high=1., scaler=1000.)
+
+        # Constrain the x and y coordinates of the fuselage to be less than .5m
+        self.driver.add_parameter('geometry.x_fueslage', low=0., high=.5, scaler=10.)
+        self.driver.add_parameter('geometry.y_fueslage', low=0., high=.5, scaler=10.)
+
+        # Constrain the fuselage length to be between 0 and 2 m
+        self.driver.add_parameter('geometry.fuselage_length', low=0., high=2.)
+
+        # Connect the density of the air to the ouptut of the atmospheric conditions
+        self.connect('conditions.rho', 'geometry.rho')
+
+        # Constrain the aircraft to fly between 5 and 15 m/s
+        self.driver.add_parameter('geometry.u_inf', low=5., high=15.)
+
+        # Constrain the angle of attack of the airfoil to be between -2 and 8 degrees
+        self.driver.add_parameter('geometry.alpha', low=-2., high=8.)
+
+
+
 
         # Iteration Hierarchy
-        #self.driver.workflow.add(['airfoil', 'conditions'])
-        self.driver.workflow.add('conditions')
+        self.driver.workflow.add(['conditions', 'geometry'])
 
         # Objective
         self.driver.add_objective('conditions.rho')
